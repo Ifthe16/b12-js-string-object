d5233851b0cc03261b3f772fb35a5bac7b11dc57
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-07 18:25:55 +0600

    added substring.js and substr.js files

---
+++ substr.js
@@ -0,0 +1,6 @@
+const address = 'andorkilla';
+
+console.log(address.substr(3, 3));
+console.log(address.substr(4, 2));
+console.log(address.substr(6, 1));
+console.log(address.substr(-4, 4));
---
+++ substring.js
@@ -0,0 +1,5 @@
+const address = 'andorkilla';
+
+console.log(address.substring(0, 5));
+console.log(address.substring(5));
+console.log(address.substring(6, 3));
