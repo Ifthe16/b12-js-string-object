ae2ae3ea5c3c2ce212e9f2604adc80cf0fa0308f
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-08 16:49:39 +0600

    added concat.js and includes.js files

---
+++ concat.js
@@ -0,0 +1,13 @@
+const first = 'Abid';
+const last = 'Nabid';
+
+const fullName = first + last;
+console.log(fullName);
+const fullName2 = first + ' ' + last;
+console.log(fullName2);
+const fullName3 = first.concat(last);
+console.log(fullName3);
+const fullName4 = first.concat(' ').concat(last);
+console.log(fullName4);
+const fullName5 = first.concat(' ', last);
+console.log(fullName5);
---
+++ includes.js
@@ -0,0 +1,6 @@
+// includes()
+const first = 'Abid';
+const last = 'Nabid';
+
+console.log(last.includes('d'));
+console.log(last.includes('x'));
