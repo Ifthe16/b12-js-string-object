9964043fcce5959926212c4013de8aaf6e1d373a
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-08 16:35:38 +0600

    added slice.js and split.js

---
+++ split.js
@@ -0,0 +1,9 @@
+const sentence = 'I am a good and hardworking person.';
+
+console.log(sentence.split(''));
+console.log(sentence.split(' '));
+console.log(sentence.split('a'));
+
+const friendStr = 'Rahim,kahim,dahim,lahim,fahim,sahim';
+const friends = friendStr.split(',');
+console.log(friends);
