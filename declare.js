a7becee3d1ce49f5b390ae4f1c57bbaab401adb3
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-10 21:40:17 +0600

    added loop.js and declare.js files

---
+++ declare.js
@@ -0,0 +1,21 @@
+const pen = { brand: 'econo', price: 10, color: 'black' }
+console.log(pen);
+
+
+const pencil = new Object();
+pencil.brand = 'Nataraj';
+pencil.price = 10;
+pencil.isNew = true;
+console.log(pencil);
+
+
+// const rubber = Object.create(null);
+const rubber = Object.create({});
+rubber.brand = "metador";
+rubber.color = "red";
+
+
+console.log(rubber);
+
+// using class or function
+// using new keyword
